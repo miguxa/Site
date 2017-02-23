33fbf66e3180c7bc125a1933cc669b14fed4d8bf
Miguel Prego <miguel_prego1992@hotmail.com>
2017-02-23 18:24:32 +0000

    Mapa bonito!

--- home.php
+++ home.php
@@ -1,44 +1,90 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Miguel Prego</title>
-		<link rel="icon" href="SSS.png" type="image/gif" sizes="16x16">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	</head>
-	
-	<body>
-		<h1>Smart Shock	web site</h1>
-		<img src="SSS.png" style="width:200px;height:200px;">
-		<h3>Please enter coordinates</h3>
-		<form role="form" method="post" action="store.php">
-			<ul style="width: 30%"> 
-				<li style="width: 340px">
-					<div class="form-group has-feedback">
-						<label>Longitude:</label>
-						<br>
-						North <input type="radio" name="Lat1" value="North" checked="checked">
-						South <input type="radio" name="Lat1" value="South">
-						<br>
-						<input class="element text" placeholder="38"   size="3" type="text" name="Lat2" value=""/>
-						<input class="element text" placeholder="6608" size="5" type="text" name="Lat3" value=""/>
-					</div>
-				</li>
-				<li style="width: 340px">
-					<div class="form-group has-feedback"> 
-						<label>Latitude:</label>
-						<br>
-						East <input type="radio" name="Lon1" value="East">
-						West <input type="radio" name="Lon1" value="West" checked="checked">
-						<br>
-						<input class="element text" placeholder="9"    size="3" type="text" name="Lon2" value=""/>
-						<input class="element text" placeholder="2049" size="5" type="text" name="Lon3" value=""/>
-					</div>
-				</li>
-				<input type="submit" class="btn btn-primary" name="val" value="Submit">
-			</ul>
-		</form>
-	
-		<!-- <img src="Mapa_FCT.PNG" alt="O Mapa" style="width: 50%; height: 50%"> -->	
-	</body>
-</html>
+<head>
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+	<meta charset="utf-8">
+	<link rel="icon" href="SSS.png" type="image/gif" sizes="16x16">
+	<title>Mapa Total</title>
+	<style>
+      /* Always set the map height explicitly to define the size of the div
+      * element that contains the map. */
+      #map {
+      	height: 90%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+      	height: 90%;
+      	margin: 1%;
+      	padding: 0;
+      }
+  </style>
+</head>
 
+<body>
+	<img src="SS.png" style="height:20%;">
+	<!--<form>
+		<input type="radio" name="colors" value=1 id="um">1<br>
+		<input type="radio" name="colors" value=2 id="2">2<br>
+	</form>-->
+	<script>
+		var locations = [];
+		var labels = [];	
+	</script>
+	<?php
+	$connect = mysqli_connect('localhost', 'root', '') or die('Não foi possível conectar: ' . mysql_error());
+	mysqli_select_db($connect, 'tese') or die('Não foi possível seleccionar o banco da dados');
+	/*if (isset($gender) && $gender=="male") {
+		$query = 'SELECT FORCA, LATITUDE, LONGITUDE FROM COORDENADAS WHERE FORCA = 1';
+	}
+	else {*/
+		$query = 'SELECT FORCA, LATITUDE, LONGITUDE FROM COORDENADAS';
+	//}
+	$result = mysqli_query ($connect, $query);
+	$number = mysqli_num_rows($result);
+
+	$i=0;
+	if(mysqli_num_rows($result)>0)        
+	while($rows = mysqli_fetch_array($result)){
+	echo '
+	<script>
+		locations['.$i.']={lat:'.$rows["LATITUDE"].' , lng: '.$rows["LONGITUDE"].'};
+		labels['.$i.']="'.$rows["FORCA"].'";
+	</script>';
+	$i = $i + 1;
+}
+?>
+
+<div id="map"></div>
+<script>
+
+	function initMap() {
+
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 20,
+			center: {lat: 38.7188, lng: -9.3315}
+		});
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+        	return new google.maps.Marker({
+        		position: location,
+        		label: labels[i]
+        	});
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+        	{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    }
+
+
+</script>
+<script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+</script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAWSJAYNSvMH3f_65A-xMS2gbGipI90Ehg&callback=initMap">
+</script>
+</body>
+</html>
--- showLocation.php
+++ showLocation.php
@@ -12,7 +12,7 @@
 	<div style="width:800px;max-width:100%;overflow:hidden;height:800px;">
 			<div id="embedded-map-display" style="height:100%; width:100%;max-width:100%;">
 				<iframe style="height:100%;width:100%;border:0;" frameborder="0" 
-				src="https://www.google.com/maps/embed/v1/place?q=<?php echo $latitude;?>,<?php echo $longitude;?>&key=AIzaSyAN0om9mFmy1QN6Wf54tXAowK4eT0ZUPrU">
+				src="https://www.google.com/maps/embed/v1/place?q=<?php echo $latitude;?>,<?php echo $longitude;?>&key=AIzaSyAWSJAYNSvMH3f_65A-xMS2gbGipI90Ehg">
 				</iframe>
 			</div>
 			<a class="embedded-map-code" rel="nofollow" href="http://www.szablonypremium.pl" id="get-data-for-embed-map">szablonypremium.pl</a>
